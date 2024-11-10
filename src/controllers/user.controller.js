4dce0a7893ef390388b1536824014fd0b67f31ec
Fida ur Rahman <fida.r@streamedinc.com>
2024-11-10 22:32:56 +0500

    change password

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -205,17 +205,12 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 })
 
 const changeCurrentPassword = asyncHandler(async(req, res) => {
-    const {oldPassword, newPassword} = req.body
+    const { newPassword, email} = req.body
 
-    
-
-    const user = await User.findById(req.user?._id)
-    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
-
-    if (!isPasswordCorrect) {
-        throw new ApiError(400, "Invalid old password")
+    const user = await User.findOne({email})
+    if(!user) {
+        throw new ApiError(404, "User does not exist")
     }
-
     user.password = newPassword
     await user.save({validateBeforeSave: false})
 
